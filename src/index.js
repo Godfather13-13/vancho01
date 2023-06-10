8d47411abd7c33d84414fe2711ddccb7b770b9f7
Godfather13-13 <132759068+Godfather13-13@users.noreply.github.com>
2023-06-10 21:52:14 +0200

    1
    2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,62 @@
-import Player  from '@vimeo/player';
-import throttle from 'lodash.throttle';
+
+import throttle from  "lodash.throttle";
+
+const ref = {
+form : document.querySelector('.feedback-form'),
+email :  document.querySelector(`input[name="email"]`),
+message : document.querySelector(`textarea[name="message"]`)
+};
 
 
-const videoPlayer = new Player('vimeo-player');
+const STORAGE_KEYS = {
+    email: 'review_email',
+    messageRev: 'review_msg',
+  };
 
 
-videoPlayer.on('timeupdate', throttle((time) => {
-
-    localStorage.setItem("videoplayer-current-time", time.hour);
-
-
-}, 1000));
+ref.form.addEventListener("submit", onFormSubmit )
+ref.email.addEventListener("input", throttle(onEmailInput, 11500));
+ref.message.addEventListener("input", throttle(onTextAreaInput, 500));
 
 
-videoPlayer.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+populateTexarea();
+
+populateEmail();
+
+function onFormSubmit(evt){
+    evt.preventDefault();
+    const formData = {
+       email : ref.email.value,
+      message : ref.message.value 
+};
+console.log(formData)
+evt.target.reset()
+localStorage.removeItem(STORAGE_KEYS.email);
+localStorage.removeItem(STORAGE_KEYS.messageRev);
+};
+
+
+
+function onEmailInput(evt){
+    const messageRev = evt.target.value;
+    localStorage.setItem(STORAGE_KEYS.email, messageRev);
+};
+
+function onTextAreaInput(evt){
+    const messageRev = evt.target.value;
+    localStorage.setItem(STORAGE_KEYS.messageRev, messageRev );
+};
+
+function populateEmail(){
+    const saveEmail = localStorage.getItem(STORAGE_KEYS.email);
+    if (saveEmail){
+        ref.email.value = saveEmail
+    }
+}
+
+function populateTexarea(){
+    const saveTextarea = localStorage.getItem(STORAGE_KEYS.messageRev)
+    if (saveTextarea){
+        ref.message.value = saveTextarea
+    }
+}
